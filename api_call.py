ec4e93908f630fc9f1d25f5ad1fe7912c937a6ff
ConstancaElias <constanca.elias@gmail.com>
2021-04-22 19:59:48 +0100

    Add script to get api data

---
+++ api_call.py
@@ -0,0 +1,37 @@
+import requests, time, json
+
+savedResponses = dict()
+allresp = list()
+api = "https://api.tomtom.com/traffic/services/"
+key = "MrTS8F1tctiivu5vgR5SV2ypkrFfwNFY"
+lat = "41.56067"
+lon = "-8.39609"
+api_trafic = "4/flowSegmentData/relative0/10/json?point="+ lat +"%2C" + lon+ "&key="+ key
+
+def iterateOverResponse(dados):
+     for atributte, value in dados['flowSegmentData'].items():
+        if atributte == 'coordinates': 
+            break
+        else:
+            savedResponses[atributte] = value
+
+def writeInFile():
+    for dic in allresp:
+        app_json = json.dumps(allresp)
+    with open("data.json", 'a') as file:
+        file.write(app_json)
+    
+
+def main():
+
+    response = requests.get(api + api_trafic)
+
+    while True:
+        print("Getting response")
+        iterateOverResponse(response.json())
+        allresp.append(savedResponses)
+        writeInFile()
+        time.sleep(900) # Delay for 15 minute (900 seconds).
+
+if __name__ == "__main__":
+    main()
